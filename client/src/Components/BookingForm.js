eb55c80628e59b8a866cc39d844021db78aded35
LaurenLingham <lauren.lingham@outlook.com>
2023-03-22 20:19:47 +0000

    fixed radio buttons for checking in

--- client/src/Components/BookingForm.js
+++ client/src/Components/BookingForm.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { postBooking } from "./BookingService";
 
 const BookingForm = ({ addBooking }) => {
-    const [formData, setFormData] = useState({});
+    const [formData, setFormData] = useState({checked_in: "false"});
 
     const onChange = (e) => {
-        formData[e.target.id] = e.target.value;
+        formData[e.target.name] = e.target.value;
         setFormData(formData);
     };
 
@@ -24,27 +24,27 @@ const BookingForm = ({ addBooking }) => {
                 <div>
                     <label htmlFor="date">Date: </label>
                     <br></br>
-                    <input type="date" id="date" name="date" onChange={onChange} value={formData.date}/>
+                    <input type="date" id="date" name="date" onChange={onChange} value={formData.date} required />
                 </div>
                 <div>
                     <label htmlFor="name">Name: </label>
                     <br></br>
-                    <input type="text" id="name" name="name" onChange={onChange} value={formData.name} required/>
+                    <input type="text" id="name" name="name" onChange={onChange} value={formData.name} required />
                 </div>
                 <div>
                     <label htmlFor="email">Email address: </label>
                     <br></br>
-                    <input type="text" id="email" name="email" onChange={onChange} value={formData.email} required/>
+                    <input type="text" id="email" name="email" onChange={onChange} value={formData.email} required />
                 </div>
                 <div>
                     <label htmlFor="checked_in">Checked in</label>
-                    <input type="radio" checked={formData.checked_in === true} id="checked_in" name="checked_in" onChange={onChange} value="true"/>
+                    <input type="radio" id="checked_in" name="checked_in" onChange={onChange} value="true" />
                 </div>
                 <div>
-                    <label htmlFor="checked_in">Checked out</label>
-                    <input type="radio" checked={formData.checked_in === false} id='checked_in' name='checked_in' onChange={onChange} value="false"/>
+                    <label htmlFor="checked_out">Checked out</label>
+                    <input type="radio" defaultChecked id="checked_out" name="checked_in" onChange={onChange} value="false" />
                 </div>
-                <input type="submit" value="Add booking"/>
+                <input type="submit" value="Add booking" />
             </form>
         </>
     );
